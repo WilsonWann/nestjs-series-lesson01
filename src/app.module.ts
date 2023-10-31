0f41e32c2de289293a4d9a0c087cfd9a1394cc37
musense <musense.ad@gmail.com>
2023-10-31 15:42:39 +0800

    add middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TestModule } from './test/test.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filter/http-exception/http-exception.filter';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
+import { HelloWorldMiddleware } from './middleware/hello-world/hello-world.middleware';
 
 @Module({
   imports: [UsersModule, TestModule],
@@ -17,4 +19,16 @@ import { HttpExceptionFilter } from './filter/http-exception/http-exception.filt
     // }
   ],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        LoggerMiddleware,
+        HelloWorldMiddleware
+      )
+      .forRoutes(
+        { path: '/test', method: RequestMethod.POST },
+        { path: '/', method: RequestMethod.GET },
+      )
+  }
+}
