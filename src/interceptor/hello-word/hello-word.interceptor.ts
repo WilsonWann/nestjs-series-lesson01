b297c3205ab5146f2e003d053c756ada414b8959
musense <musense.ad@gmail.com>
2023-10-31 16:57:40 +0800

    apply helloWorldInterceptor to testController

--- src/interceptor/hello-word/hello-word.interceptor.ts
+++ src/interceptor/hello-word/hello-word.interceptor.ts
@@ -1,9 +1,17 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { tap } from "rxjs/operators";
+
 
 @Injectable()
 export class HelloWordInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    console.log('Hello World!');
+    const input = Date.now()
+    const handler = next.handle()
+    return handler.pipe(
+      tap(() => console.log(`${Date.now() - input} ms`))
+    )
+
   }
 }
