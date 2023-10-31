a458d044dd66f73782e681ea4c536a8218f6ed23
musense <musense.ad@gmail.com>
2023-10-31 14:17:23 +0800

    move createTestDto to dto/createTestDto

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,7 +1,7 @@
 import { HttpException, Controller, Get, Post, Patch, Delete, Param, Body, Response, HttpStatus, UsePipes, UseFilters } from '@nestjs/common';
 import { TestService } from './test.service';
 import { TestPipe } from './test.pipe';
-import { CreateTestDto } from './create-test.dto';
+import { CreateTestDto } from '../dto/create-test.dto';
 import { ParseIntPipe } from 'src/pipe/parse-int/parse-int.pipe';
 
 @Controller('test')
