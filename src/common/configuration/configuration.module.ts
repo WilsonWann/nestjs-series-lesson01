4fc9a4d338c540ac488b13959e9c675776c2f58c
musense <musense.ad@gmail.com>
2023-10-31 17:53:08 +0800

    update common/configuration module & service

--- src/common/configuration/configuration.module.ts
+++ src/common/configuration/configuration.module.ts
@@ -1,12 +1,17 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigurationService } from './configuration.service';
+import { ENV_PATH } from './constants/token.const';
 
 @Module({})
 export class ConfigurationModule {
 
-  static forRoot(): DynamicModule {
+  static forRoot(options: { path: string }): DynamicModule {
     return {
       providers: [
+        {
+          provide: ENV_PATH,
+          useValue: options.path
+        },
         ConfigurationService
       ],
       module: ConfigurationModule,
--- src/common/configuration/configuration.service.ts
+++ src/common/configuration/configuration.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ENV_PATH } from './constants/token.const';
+import * as fs from "fs";
+import * as path from "path";
+import * as dotenv from "dotenv";
+
+
 
 @Injectable()
-export class ConfigurationService {}
+export class ConfigurationService {
+
+  private config: any;
+
+  constructor(@Inject(ENV_PATH) private readonly path: string) {
+    this.setEnvironment()
+  }
+
+  public get(key: string): string {
+    return this.config[key]
+  }
+
+  private setEnvironment(): void {
+    const filePath = path.resolve(__dirname, '../../', this.path)
+    this.config = dotenv.parse(fs.readFileSync(filePath))
+  }
+
+
+}
