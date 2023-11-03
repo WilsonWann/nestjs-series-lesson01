113db2a51740d84b80df4544ece3c5760c1f258a
musense <musense.ad@gmail.com>
2023-11-03 17:57:03 +0800

    fileUpload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { FileInterceptor, FilesInterceptor, FileFieldsInterceptor, AnyFilesInterceptor } from '@nestjs/platform-express';
 
 
 
@@ -14,5 +15,24 @@ export class AppController {
     const app_domain = this.configService.get('APP_DOMAIN');
     const redirect_url = this.configService.get('APP_REDIRECT_URL');
     return { app_domain, redirect_url }
+  }
+
+  @Post('/multiple')
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'first' },
+    { name: 'second' },
+  ]))
+
+  uploadSingleFile(@UploadedFiles() files: { [x: string]: Express.Multer.File[] }) {
+    const { first, second } = files
+    const list = [...first, ...second]
+    return list.map(({ fieldname, originalname }) => ({ fieldname, originalname }))
+
+  }
+  @Post('/multiple/any')
+  @UseInterceptors(AnyFilesInterceptor())
+  uploadSingleFileAny(@UploadedFiles() files: Express.Multer.File[]) {
+    return files.map(({ fieldname, originalname }) => ({ fieldname, originalname }))
+
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configurationFactory from 'config/configuration.factory';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { MulterHelper } from './core/helpers/multer.helper';
 
 @Module({
   imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: MulterHelper.destination,
+        filename: MulterHelper.filenameHandler
+      })
+    }),
     ConfigModule.forRoot({
       envFilePath: [
         '.development.local.env',
